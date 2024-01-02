e0531b3f51138ba5dc7fcb050708239afeafba43
tsikhankaras <tsikhankaras@gmail.com>
2024-01-02 17:19:13 +0300

    refactor(basic-3.4): remove tag selectors

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var completedTasksHolder=document.getElementById("completed-tasks");//completed-
 //New task list item
 var createNewTaskElement=function(taskString){
 
-    var listItem=document.createElement("li");
-    listItem.className = "li";
+    var listItem=document.createElement("task-item");
+    listItem.className = "task-item";
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
@@ -43,9 +43,9 @@ var createNewTaskElement=function(taskString){
     editInput.className="input-text task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="button edit";
+    editButton.className="button button_edit";
 
-    deleteButton.className="button delete";
+    deleteButton.className="button button_delete";
     deleteButtonImg.src='./remove.svg';
     deleteButton.appendChild(deleteButtonImg);
 
@@ -86,7 +86,7 @@ var editTask=function(){
 
     var editInput=listItem.querySelector('.input-text');
     var label=listItem.querySelector("label");
-    var editBtn=listItem.querySelector(".edit");
+    var editBtn=listItem.querySelector(".button_edit");
     var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
@@ -158,8 +158,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
     console.log("bind list item events");
 //select ListItems children
     var checkBox=taskListItem.querySelector("input[type=checkbox]");
-    var editButton=taskListItem.querySelector("button.edit");
-    var deleteButton=taskListItem.querySelector("button.delete");
+    var editButton=taskListItem.querySelector("button.button_edit");
+    var deleteButton=taskListItem.querySelector("button.button_delete");
 
 
     //Bind editTask to edit button.
